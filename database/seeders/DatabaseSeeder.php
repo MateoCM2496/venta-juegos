c91df46f3830264373d4f6fe40d3a4245862308b
MateoCM96 <mateo.cifuentes@utp.edu.co>
2023-11-21 16:43:26 -0500

    Migraciones para rol admin con bouncer

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,13 +4,16 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+
+use Bouncer;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
         // \App\Models\User::factory(10)->create();
 
@@ -18,5 +21,33 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        $users = [
+            [
+                'name' => 'admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('12345678'),
+
+            ]
+        ];
+
+        foreach ($users as $user) {
+            User::create($user);
+        }
+
+        $admin = Bouncer::role()->firstOrCreate([
+            'name' => 'admin',
+            'title' => 'Administrator',
+        ]);
+        
+        $admin_abilities = Bouncer::ability()->firstOrCreate([
+            'name' => 'create-games',
+            'title' => 'Create new game ',
+        ]);
+        
+        Bouncer::allow($admin)->to($admin_abilities);
+
+        $user = User::first();
+
+        Bouncer::assign('admin')->to($user);
     }
 }
