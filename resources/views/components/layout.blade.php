f2f918f16c44987d9881e32940461c26679b31c6
TheLlamitas <nicolas.llamas@utp.edu.co>
2023-11-21 22:42:24 -0500

    Implementacion del logout en el panel de control

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -22,11 +22,7 @@
             <div class="flex md:order-2">
                 <div class="relative hidden md:block">
                     <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
+                        <x-icons.search/>
                         <span class="sr-only">Search icon</span>
                     </div>
                     <input type="text" id="search-navbar"
--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,6 +1,21 @@
 <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <div>
-        <h1 class="text-3xl font-bold">Panel de Control</h1>
+    <div class="flex justify-start items-center pr-5">
+        <h1 class="text-2xl font-bold">Panel de Control</h1>
     </div>
-    
+    <div class="flex justify-center items-center pr-5">
+        <input type="text" id="search-navbar"
+            class="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Buscar...">
+    </div>
+    <div class="flex items-center">
+        @if (Auth::check())
+        <a href="{{ route('logout') }}"
+            class="flex text-black bg-gray-100 bg-transparent md:p-0 dark:text-black md:dark:text-black dark:bg-gray-700  md:dark:bg-transparent dark:border-gray-700"
+            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Cerrar Sesion
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf <!-- Agrega el token CSRF -->
+        </form>
+        @endif
+    </div>
 </nav>
