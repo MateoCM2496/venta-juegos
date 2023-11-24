ff57cbc0ff6dc6b87fef20bbb8319e782d30e841
MateoCM96 <mateo.cifuentes@utp.edu.co>
2023-11-24 11:14:30 -0500

    una correccion

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'image',
+        'gender',
+        'price',
+        'stock',
+    ];
+
+    public function game()
+    {
+        return $this->hasMany(Game::class);
+    }
 }
